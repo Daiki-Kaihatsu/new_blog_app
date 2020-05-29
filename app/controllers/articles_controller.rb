1bff7f7f4d9ef0664d67f20203124971e8f805d0
Daichan <daichan.kaihatsu@gmail.com>
2020-05-29 04:43:19 +0000

    画像投稿とプレビュー機能の実装

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,61 @@
+class ArticlesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_article, only: [:show,:edit,:update,:destroy]
+  before_action :validate_user, only: [:show,:edit,:update,:destroy]
+  
+  def index
+    @articles = Article.order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    @article = Article.new(article_params)
+    @article.user_id = current_user.id
+    if @article.save
+      redirect_to @article, notice:'作成できました'
+    else
+      render :new, alert:'作成出来ませんでした'
+    end
+  end
+
+  def edit
+  end
+  
+  def update
+    if @article.update(article_params)
+      redirect_to @article, notice:'更新できました'
+    else
+      render :edit, alert:'更新出来ませんでした'
+    end
+  end
+  
+  def destroy
+    if @article.destroy
+      redirect_to root_path, notice:'削除に成功しました'
+    else
+      redirect_to root_path, alert:'削除に失敗しました'
+
+    end
+  end
+  
+  private
+    
+    def find_article
+      @article = Article.find(params[:id])
+    end
+    
+    def article_params
+      params.require(:article).permit(:title,:body,:image)
+    end
+    
+    def validate_user
+ 
+    end
+  
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '2965f4636dc2e3cc54a5e61ebd912b877d838af7a36c2d7b98b262ab5ba9ed3277df2e52d146c4b1459bf9524d2483bf4ccab770b2707118c23bf8d249050a41'
